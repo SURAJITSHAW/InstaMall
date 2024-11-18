323cd02ed60d65141c5c2c96cd12b831599ce0f8
SURAJITSHAW <piklu3043@gmail.com>
2024-11-18 07:54:16 +0530

    set up botttom nav

--- app/src/main/java/com/example/instamall/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/instamall/activities/ShoppingActivity.kt
@@ -5,17 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.instamall.R
+import com.example.instamall.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityShoppingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_shopping)
+//        enableEdgeToEdge()
+        binding = ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
