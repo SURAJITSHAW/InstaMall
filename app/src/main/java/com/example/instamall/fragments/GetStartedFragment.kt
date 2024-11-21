0cbcd3ba367a55284574ac2261b94d43678650ec
SURAJITSHAW <piklu3043@gmail.com>
2024-11-21 15:27:09 +0530

    until cart functionality

--- app/src/main/java/com/example/instamall/fragments/GetStartedFragment.kt
+++ app/src/main/java/com/example/instamall/fragments/GetStartedFragment.kt
@@ -1,5 +1,6 @@
 package com.example.instamall.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.instamall.R
+import com.example.instamall.activities.ShoppingActivity
 import com.example.instamall.databinding.FragmentGetStartedBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class GetStartedFragment : Fragment() {
     private var _binding: FragmentGetStartedBinding? = null
     private val binding get() = _binding!!
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,12 @@ class GetStartedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentGetStartedBinding.inflate(inflater, container, false)
+
+        // Check if the user is already authenticated
+        if (auth.currentUser != null) {
+            startActivity(Intent(requireContext(), ShoppingActivity::class.java))
+            requireActivity().finish()
+        }
 
         // Set up navigation to RegisterFragment when "Register" link is clicked
         binding.register.setOnClickListener {
